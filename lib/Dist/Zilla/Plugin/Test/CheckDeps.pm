84a9295909eef13995480bfd46237b4576a3d160
Leon Timmermans <fawaka@gmail.com>
2012-05-02 19:56:29 +0000

    Import of LEONT/Dist-Zilla-Plugin-Test-CheckDeps-0.004 from CPAN.
    gitpan-cpan-distribution: Dist-Zilla-Plugin-Test-CheckDeps
    gitpan-cpan-version:      0.004
    gitpan-cpan-path:         LEONT/Dist-Zilla-Plugin-Test-CheckDeps-0.004.tar.gz
    gitpan-cpan-author:       LEONT
    gitpan-cpan-maturity:     released

--- lib/Dist/Zilla/Plugin/Test/CheckDeps.pm
+++ lib/Dist/Zilla/Plugin/Test/CheckDeps.pm
@@ -1,11 +1,11 @@
 package Dist::Zilla::Plugin::Test::CheckDeps;
 {
-  $Dist::Zilla::Plugin::Test::CheckDeps::VERSION = '0.003';
+  $Dist::Zilla::Plugin::Test::CheckDeps::VERSION = '0.004';
 }
 
 use Moose;
 extends qw/Dist::Zilla::Plugin::InlineFiles/;
-with qw/Dist::Zilla::Role::TextTemplate/;
+with qw/Dist::Zilla::Role::TextTemplate Dist::Zilla::Role::PrereqSource/;
 
 has fatal => (
 	is => 'ro',
@@ -23,6 +23,11 @@ around add_file => sub {
 	);
 };
 
+sub register_prereqs {
+	my $self = shift;
+	$self->zilla->register_prereqs({ phase => 'test' }, 'Test::More' => 0.88, 'Test::CheckDeps' => 0.002);
+}
+
 __PACKAGE__->meta->make_immutable;
 no Moose;
 
@@ -38,7 +43,7 @@ Dist::Zilla::Plugin::Test::CheckDeps
 
 =head1 VERSION
 
-version 0.003
+version 0.004
 
 =head1 AUTHOR
 
@@ -79,4 +84,10 @@ __END__
 =head1 DESCRIPTION
 
 This module adds a test that assures all dependencies have been installed properly. If requested, it can bail out all testing on error.
+
+__END__
+
+=for Pod::Coverage
+register_prereqs
+=end
 
--- t/00-check-deps.t
+++ t/00-check-deps.t
@@ -4,7 +4,7 @@ use Test::CheckDeps;
 check_dependencies();
 
 if (0) {
-	BAIL_OUT("Missing dependencies") if !Test::More->builder->is_passing;
+    BAIL_OUT("Missing dependencies") if !Test::More->builder->is_passing;
 }
 
 done_testing;
