3c4fd172b1000680fe39ab79cdd71577ce090f98
Karen Etheridge <ether@cpan.org>
2013-09-19 01:57:47 +0000

    Import of ETHER/Dist-Zilla-Plugin-Test-CheckDeps-0.008 from CPAN.
    gitpan-cpan-distribution: Dist-Zilla-Plugin-Test-CheckDeps
    gitpan-cpan-version:      0.008
    gitpan-cpan-path:         ETHER/Dist-Zilla-Plugin-Test-CheckDeps-0.008.tar.gz
    gitpan-cpan-author:       ETHER
    gitpan-cpan-maturity:     released

--- lib/Dist/Zilla/Plugin/Test/CheckDeps.pm
+++ lib/Dist/Zilla/Plugin/Test/CheckDeps.pm
@@ -1,44 +1,52 @@
 package Dist::Zilla::Plugin::Test::CheckDeps;
-{
-  $Dist::Zilla::Plugin::Test::CheckDeps::VERSION = '0.007';
+BEGIN {
+  $Dist::Zilla::Plugin::Test::CheckDeps::AUTHORITY = 'cpan:ETHER';
 }
+{
+  $Dist::Zilla::Plugin::Test::CheckDeps::VERSION = '0.008';
+}
+# git description: v0.007-7-gd327c95
+
+# vim: set ts=4 sw=4 tw=78 et nolist :
 
 use Moose;
 extends qw/Dist::Zilla::Plugin::InlineFiles/;
 with qw/Dist::Zilla::Role::TextTemplate Dist::Zilla::Role::PrereqSource/;
 
 has fatal => (
-	is => 'ro',
-	isa => 'Bool',
-	default => 0,
+    is => 'ro',
+    isa => 'Bool',
+    default => 0,
 );
 
 has level => (
-        is => 'ro',
-        isa => 'Str',
-        lazy => 1,
-        default => 'classic',
+    is => 'ro',
+    isa => 'Str',
+    lazy => 1,
+    default => 'classic',
 );
 
 
 around add_file => sub {
-	my ($orig, $self, $file) = @_;
+    my ($orig, $self, $file) = @_;
 
-	return $self->$orig(
-		Dist::Zilla::File::InMemory->new(
-			name    => $file->name,
-			content => $self->fill_in_string($file->content,
-                                         {
-                                                fatal => $self->fatal,
-                                                level => $self->level,
-                                         })
-		)
-	);
+    return $self->$orig(
+        Dist::Zilla::File::InMemory->new(
+            name    => $file->name,
+            content => $self->fill_in_string($file->content,
+            {
+                dist => \($self->zilla),
+                plugin => \$self,
+                fatal => $self->fatal,
+                level => $self->level,
+            })
+        )
+    );
 };
 
 sub register_prereqs {
-	my $self = shift;
-	$self->zilla->register_prereqs({ phase => 'test' }, 'Test::More' => 0.94, 'Test::CheckDeps' => 0.006);
+    my $self = shift;
+    $self->zilla->register_prereqs({ phase => 'test' }, 'Test::More' => 0.94, 'Test::CheckDeps' => 0.006);
 }
 
 __PACKAGE__->meta->make_immutable;
@@ -50,13 +58,17 @@ no Moose;
 
 =pod
 
+=encoding utf-8
+
+=for :stopwords Leon Timmermans Karen Etheridge
+
 =head1 NAME
 
 Dist::Zilla::Plugin::Test::CheckDeps - Check for presence of dependencies
 
 =head1 VERSION
 
-version 0.007
+version 0.008
 
 =head1 SYNOPSIS
 
@@ -87,12 +99,31 @@ This software is copyright (c) 2011 by Leon Timmermans.
 This is free software; you can redistribute it and/or modify it under
 the same terms as the Perl 5 programming language system itself.
 
+=head1 CONTRIBUTORS
+
+=over 4
+
+=item *
+
+Karen Etheridge <ether@cpan.org>
+
+=item *
+
+Leon Timmermans <fawaka@gmail.com>
+
+=back
+
 =cut
 
 __DATA__
 ___[ t/00-check-deps.t ]___
+use strict;
+use warnings;
+
+# this test was generated with {{ ref($plugin) . ' ' . ($plugin->VERSION || '<self>') }}
+
 use Test::More 0.94;
-use Test::CheckDeps 0.004;
+use Test::CheckDeps 0.007;
 
 check_dependencies('{{ $level }}');
 
