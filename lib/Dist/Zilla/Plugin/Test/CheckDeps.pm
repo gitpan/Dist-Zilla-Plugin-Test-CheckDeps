3acd04727a9ea940ba9cbf59bac19450ce948cf6
Karen Etheridge <ether@cpan.org>
2013-09-26 03:39:56 +0000

    Import of ETHER/Dist-Zilla-Plugin-Test-CheckDeps-0.009 from CPAN.
    gitpan-cpan-distribution: Dist-Zilla-Plugin-Test-CheckDeps
    gitpan-cpan-version:      0.009
    gitpan-cpan-path:         ETHER/Dist-Zilla-Plugin-Test-CheckDeps-0.009.tar.gz
    gitpan-cpan-author:       ETHER
    gitpan-cpan-maturity:     released

--- lib/Dist/Zilla/Plugin/Test/CheckDeps.pm
+++ lib/Dist/Zilla/Plugin/Test/CheckDeps.pm
@@ -3,15 +3,16 @@ BEGIN {
   $Dist::Zilla::Plugin::Test::CheckDeps::AUTHORITY = 'cpan:ETHER';
 }
 {
-  $Dist::Zilla::Plugin::Test::CheckDeps::VERSION = '0.008';
+  $Dist::Zilla::Plugin::Test::CheckDeps::VERSION = '0.009';
 }
-# git description: v0.007-7-gd327c95
+# git description: v0.008-9-g4be7e67
 
 # vim: set ts=4 sw=4 tw=78 et nolist :
 
 use Moose;
 extends qw/Dist::Zilla::Plugin::InlineFiles/;
 with qw/Dist::Zilla::Role::TextTemplate Dist::Zilla::Role::PrereqSource/;
+use namespace::autoclean;
 
 has fatal => (
     is => 'ro',
@@ -26,13 +27,18 @@ has level => (
     default => 'classic',
 );
 
+has filename => (
+    is => 'ro',
+    isa => 'Str',
+    default => 't/00-check-deps.t',
+);
 
 around add_file => sub {
     my ($orig, $self, $file) = @_;
 
     return $self->$orig(
         Dist::Zilla::File::InMemory->new(
-            name    => $file->name,
+            name    => $self->filename,
             content => $self->fill_in_string($file->content,
             {
                 dist => \($self->zilla),
@@ -46,15 +52,12 @@ around add_file => sub {
 
 sub register_prereqs {
     my $self = shift;
-    $self->zilla->register_prereqs({ phase => 'test' }, 'Test::More' => 0.94, 'Test::CheckDeps' => 0.006);
+    $self->zilla->register_prereqs({ phase => 'test' }, 'Test::More' => '0.94', 'Test::CheckDeps' => '0.007');
 }
 
 __PACKAGE__->meta->make_immutable;
-no Moose;
 
-1;
-
-#ABSTRACT: Check for presence of dependencies
+# ABSTRACT: Check for presence of dependencies
 
 =pod
 
@@ -68,7 +71,7 @@ Dist::Zilla::Plugin::Test::CheckDeps - Check for presence of dependencies
 
 =head1 VERSION
 
-version 0.008
+version 0.009
 
 =head1 SYNOPSIS
 
@@ -80,13 +83,22 @@ version 0.008
 
 This module adds a test that assures all dependencies have been installed properly. If requested, it can bail out all testing on error.
 
-If C<fatal> is true, C<BAIL_OUT> is called if the tests fail.
+This plugin accepts the following options:
 
-C<level> is passed to C<check_dependencies> in L<Test::CheckDeps>.
-(Defaults to C<'classic'>.)
+=over 4
+
+=item * C<fatal>: if true, C<BAIL_OUT> is called if the tests fail. Defaults
+to false.
+
+=item * C<level>: passed to C<check_dependencies> in L<Test::CheckDeps>.
+(Defaults to C<classic>.)
+
+=item * C<filename>: the name of the generated file. Defaults to
+F<t/00-check-deps.t>.
+
+=back
 
 =for Pod::Coverage register_prereqs
-=end
 
 =head1 AUTHOR
 
@@ -116,7 +128,7 @@ Leon Timmermans <fawaka@gmail.com>
 =cut
 
 __DATA__
-___[ t/00-check-deps.t ]___
+___[ test-checkdeps ]___
 use strict;
 use warnings;
 
